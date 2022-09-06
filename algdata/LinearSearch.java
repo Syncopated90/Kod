6221b193544e5da0e2f0d01dfc44f54bcc211199
Syncopated90 <fredrikehne@yahoo.se>
2022-09-06 18:44:07 +0200

    weird behavior with second if statement in sorted search

--- algdata/LinearSearch.java
+++ algdata/LinearSearch.java
@@ -1,45 +1,75 @@
 import java.util.Random;
 class LinearSearch{
-	public void unsortedBenchmark(){
+	public void Benchmark(){
 		Random random = new Random();
-		int n = 100;
+		int n = 10000;
+		int max = 5000;
 		int key;
-		long searchTime = 0;
 		boolean dummy;
-		long t0, t1, dummyTime, newTime;
+		long searchTime = 0;
+		long t0, dummyTime, newTime;
 		int[] array;
-		while(n < 51200){
+		while(n < 1000000){
+			array = fillArray(n);
 			t0 = System.nanoTime();
-			for(int i = 0; i < 5000; i++){
-				array = fillArray(n);
-				key = random.nextInt(100);
+			for(int i = 0; i < max; i++){
+				key = random.nextInt(n * 10);
+				key++;
+			}
+			dummyTime = (System.nanoTime() - t0);
+			t0 = System.nanoTime();
+			for(int i = 0; i < max; i++){
+				key = random.nextInt(n * 10);
 				dummy = search_unsorted(array, key);
 			}
-			t1 = System.nanoTime();
-			searchTime += (t1 - t0);
+			searchTime = (System.nanoTime() - t0);
+			searchTime -= dummyTime;
+			System.out.println("Total search time for n = " + n + ": " + searchTime);
+			array = sorted(n);
 			t0 = System.nanoTime();
-			for(int i = 0; i < 5000; i++){
-				array = fillArray(n);
-				key = random.nextInt(100);
+			for(int i = 0; i < max; i++){
+				key = random.nextInt(n * 10);
+				dummy = search_sorted(array, key);
 			}
-			dummyTime = System.nanoTime() - t0;
-			newTime = searchTime - dummyTime;
-			System.out.println("Total search time for n = " + n + ": " + newTime);
+			searchTime = (System.nanoTime() - t0);
+			searchTime -= dummyTime;
+			System.out.println("Total search time for n = " + n + ": " + searchTime + " in a sorted array");
 			n *= 2;
 		}
 	}
 	public static boolean search_unsorted(int[] array, int key) {
-		for (int index = 0; index < array.length ; index++) {
+		for (int index = 0; index < array.length; index++) {
 			if (array[index] == key)
 				return true;
 		}
 		return false;
 	}
-	public int[] fillArray(int n){
+	public static boolean search_sorted(int[] array, int key) {
+		for (int index = 0; index < array.length; index++) {
+			int target = array[index];
+			/*if (target > key)
+				return false;*/
+			if (target == key)
+				return true;
+
+		}
+		return false;
+	}
+	public static int[] fillArray(int n){
 		int[] array = new int[n];
 		Random random = new Random();
 		for(int i = 0;i < array.length-1; i++)
-			array[i] = random.nextInt(100);
+			array[i] = random.nextInt(n);
 		return array;
 	}
+	private static int[] sorted(int n) {
+		Random rnd = new Random();
+		int[] array = new int[n];
+		int nxt = 0;
+		for (int i = 0; i < n ; i++) {
+			nxt += rnd.nextInt(10) + 1;
+			array[i] = nxt;
+		}
+		return array;
+}
 }
--- algdata/SortedBenchmark.java
+++
@@ -1,8 +0,0 @@
-import java.util.Random;
-class SortedBenchmark{
-	int n = 1000;
-	int[] array = new int[n];
-	public SortedBenchmark(){
-		Random random = new Random();
-	}
-}
