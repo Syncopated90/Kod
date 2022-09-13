2d70f6649247f15cbf29df44cee56b957fa74f11
Syncopated90 <fredrikehne@yahoo.se>
2022-09-13 17:09:07 +0200

    benching working, wrong (linear) complexity for merge

--- algdata/sorting/Sorting.java
+++ algdata/sorting/Sorting.java
@@ -2,19 +2,52 @@ import java.util.Random;
 import java.util.Arrays;
 class Sorting{
   public static void main(String[] args){
-    int n = 5; //array size
-    int[] array = new int[n];
-    randomize(array);
-    Selection.sort(array);
-    randomize(array);
-    Insertion.sort(array);
-    randomize(array);
-    Merge.sort(array);
+    int n = 100; //array size
+    int loop = 100; //number of time to loop the algorithms
+    benchmark(n, loop);
   }
   private static void randomize(int[] array){
     Random random = new Random();
     for(int i = 0; i < array.length; i++){
       array[i] = random.nextInt(10);
+    }
+  }
+  private static void benchmark(int n, int loop){
+    while(n <= 8192000){
+      int[] array = new int[n];
+      long t0, t1;
+      double min = Double.POSITIVE_INFINITY;
+    /*  for(int i = 0; i < loop; i++){
+          min = Double.POSITIVE_INFINITY;
+          randomize(array);
+          t0 = System.nanoTime();
+          Selection.sort(array);
+          t1 = (System.nanoTime() - t0);
+          if(t1 < min)
+            min = t1;
+      }
+      System.out.println(min/1000 + " us: time for selection sort");
+      for(int i = 0; i < loop; i++){
+        min = Double.POSITIVE_INFINITY;
+        randomize(array);
+        t0 = System.nanoTime();
+        Insertion.sort(array);
+        t1 = (System.nanoTime() - t0);
+        if(t1 < min)
+          min = t1;
+      }
+      System.out.println(min/1000 + " us: time for insertion sort");
+      */for(int i = 0; i < loop; i++){
+        min = Double.POSITIVE_INFINITY;
+        randomize(array);
+        t0 = System.nanoTime();
+        Merge.sort(array);
+        t1 = (System.nanoTime() - t0);
+        if(t1 < min)
+          min = t1;
+      }
+      System.out.println(min/1000 + " us: time for merge sort");
+      n *=2;
     }
   }
 }
