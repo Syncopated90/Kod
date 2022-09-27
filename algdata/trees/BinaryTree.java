31837f1bc80df2e4bf9e83a81eb4e624b0c774b4
Syncopated90 <fredrikehne@yahoo.se>
2022-09-27 10:05:27 +0200

    add, lookup and toString probably working

--- algdata/trees/BinaryTree.java
+++ algdata/trees/BinaryTree.java
@@ -1,39 +1,88 @@
 class BinaryTree{
   private Node root;
-  public BinaryTree(int key, int value){
+  public BinaryTree(Integer key, Integer value){
     this.root = new Node(key,value);
   }
   public BinaryTree(){
     this.root = null;
   }
   public class Node{
-    private int key;
-    private int value;
+    private Integer key;
+    private Integer value;
     private Node left, right;
 
-    public Node(int key, int value){
+    public Node(Integer key, Integer value){
       this.key = key;
       this.value = value;
       this.left = this.right = null;
     }
-    public String toString(){
-      StringBuilder sb = new StringBuilder("(");
-      sb.append(key);
+    private String valuesToString(){
+      StringBuilder sb = new StringBuilder();
+      sb.append("(");
+      sb.append(this.key);
       sb.append(", ");
-      sb.append(value);
+      sb.append(this.value);
       sb.append(")");
       return sb.toString();
     }
-
+    public String toString(){
+      StringBuilder sb = new StringBuilder();
+      if(this.left != null)
+        sb.append(this.left.toString());
+      sb.append(this.valuesToString());
+      if(this.right != null)
+        sb.append(this.right.toString());
+      return sb.toString();
+    }
+    private Integer lookup(Integer searchKey){
+      if(searchKey == this.key)
+        return this.value;
+      else if(this.left != null && searchKey < this.key)
+        return this.left.lookup(searchKey);
+      else if(this.right != null && searchKey > this.key)
+        return this.right.lookup(searchKey);
+      else
+        return null;
+    }
+    private void add(Integer key, Integer value){
+      if(key == this.key)
+        this.value = value;
+      if(key < this.key){
+        if(this.left == null){
+          this.left = new Node(key, value);
+        }
+        else
+          this.left.add(key, value);
+      }
+      if(key > this.key){
+        if(this.right == null)
+          this.right = new Node(key, value);
+        else{
+          this.right.add(key, value);
+        }
+      }
+    }
   }
 
-  public void add(int key, int value){
+  public void add(Integer key, Integer value){
     if(this.root == null)
       this.root = new Node(key,value);
+    else
+      root.add(key, value);
   }
-
+  public Integer lookup(Integer key){
+    if(this.root != null)
+      return this.root.lookup(key);
+    else
+      return null;
+  }
   public String toString(){
-    return root.toString();
+    StringBuilder sb = new StringBuilder();
+    sb.append(this.root.valuesToString());
+    if(this.root.left != null)
+      sb.append(this.root.left.toString());
+    if(this.root.right != null)
+    sb.append(this.root.right.toString());
+    return sb.toString();
   }
-
 }
--- algdata/trees/Trees.java
+++ algdata/trees/Trees.java
@@ -1,6 +1,18 @@
+import java.util.Random;
+
 class Trees{
   public static void main(String[] args){
-    BinaryTree binTree = new BinaryTree(1,1);
+    Random random = new Random();
+    BinaryTree binTree = new BinaryTree(10,10);
     System.out.println(binTree.toString());
+    binTree.add(2,4);
+    binTree.add(7,14);
+    binTree.add(1,2);
+    binTree.add(8,16);
+    binTree.add(15,30);
+    binTree.add(13,26);
+    binTree.add(14,28);
+    System.out.println(binTree.toString());
+    System.out.println(binTree.lookup(Integer.parseInt(args[0])));
   }
 }
