933af8b9b9fef916da86b9dafca1edcafb1a63d1
Syncopated90 <fredrikehne@yahoo.se>
2022-10-19 16:31:33 +0200

    add is working, search kinda not really working

--- algdata/t9/T9.java
+++ algdata/t9/T9.java
@@ -1,11 +1,41 @@
 class T9{
   public static void main(String[] args){
     Trie trie = new Trie();
-    char c = 109;//ascii m
+    trie.add("ask");
+    trie.add("bsl");
+    trie.add("asl");
+    //System.out.println(trie.search(args[0]));
+    String[] words = {"hello", "world"};
+    for(String i: words){
+      i = "world " + i;
+      System.out.println(i);
+    }
+
+    /*
+
+
+    System.out.println(trie.codeToIndex(trie.charToCode('a')));
+    trie.search("12");
+    String hello = "9234";
+    String a, b, c;
+    a = b = c = "";
+    System.out.println(c);
+    System.out.println(hello.substring(5, hello.length()));
+    if(hello.substring(0, hello.length()).length() == 0)
+      System.out.println("1134 :" + String.valueOf(1234 - 100));
+    System.out.println(Character.getNumericValue(hello.charAt(0)));
+    System.out.println();
+    if(trie.root[0] != null)
+      System.out.println(trie.codeToChar(trie.indexToCode(0)));
+    if(trie.root[trie.codeToIndex(trie.charToCode('a'))].next[trie.codeToIndex(trie.charToCode('s'))] != null)
+      System.out.println((trie.charToCode('s')));
+    System.out.println(trie.codeToChar(25));
+    System.out.println(trie.keyToIndex(trie.charToCode('c')));char c = 109;//ascii m
     int a = trie.charToCode(c) + 96;
     char b = (char) a;
     for(int i = 0; i < 27; i++)
       System.out.println(args[0].charAt(i) + " : " + trie.charToKey(args[0].charAt(i)));
-    //System.out.println(trie.charToKey('s'));
+    System.out.println(trie.charToKey('s'));
+    */
   }
 }
--- algdata/t9/Trie.java
+++ algdata/t9/Trie.java
@@ -1,5 +1,123 @@
 class Trie{
-  public int charToCode(char letter){
+  Node[] root;
+
+  public Trie(){
+    this.root = new Node[27];
+  }
+
+  public void add(String word){
+    Node[] letters = this.root;
+    for(int i = 0; i < word.length(); i++){
+      if(letters[codeToIndex(charToCode(word.charAt(i)))] == null)
+        letters[codeToIndex(charToCode(word.charAt(i)))] = new Node(false);
+      if(i == word.length() - 1)
+        letters[codeToIndex(charToCode(word.charAt(i)))].exist = true;
+      letters = letters[codeToIndex(charToCode(word.charAt(i)))].next;
+    }
+  }
+  public String search(String word){
+    int key = Character.getNumericValue(word.charAt(0));
+    String a, b, c;
+    a = b = c = "";
+    if(word.length() == 1){
+      if(this.root[(key - 1) * 3] != null && this.root[(key - 1) * 3].exist == true){
+        a = Character.toString(codeToChar((key - 1) * 3 + 1)) + " ";
+      }
+      if(this.root[((key - 1) * 3) + 1] != null && this.root[(key - 1) * 3 + 1].exist == true){
+        b = Character.toString(codeToChar((key - 1) * 3 + 2)) + " ";
+      }
+      if(this.root[((key - 1) * 3) + 2] != null && this.root[(key - 1) * 3 + 2].exist == true)
+        c = Character.toString(codeToChar((key - 1) * 3 + 3)) + " ";
+    }
+    else if(word.length() > 1){
+      if(this.root[(key - 1) * 3] != null){
+        a = this.root[(key - 1) * 3].search(word.substring(1, word.length()));
+        //if(this.root[(key - 1) * 3].exist == true)
+        if(a.length() != 0)
+          a = Character.toString(codeToChar((key - 1) * 3 + 1)) + a;
+
+      }
+      if(this.root[((key - 1) * 3 + 1)] != null){
+        b = this.root[((key - 1) * 3 + 1)].search(word.substring(1, word.length()));
+        //if(this.root[(key - 1) * 3 + 1].exist == true)
+        if(b.length() != 0)
+          b = Character.toString(codeToChar((key - 1) * 3 + 2)) + b;
+
+      }
+      if(this.root[((key - 1) * 3 + 2)] != null){
+        c = this.root[((key - 1) * 3 + 2)].search(word.substring(1, word.length()));
+        //if(this.root[(key - 1) * 3 + 2].exist == true)
+        if(c.length() != 0)
+          c = Character.toString(codeToChar((key - 1) * 3 + 3)) + c;
+      }
+    }
+    return a + b + c;
+  }
+
+
+  public class Node{
+    public Node[] next;
+    public boolean exist;
+
+    public Node(){
+      this.next = new Node[27];
+      this.exist = false;
+    }
+
+    public Node(boolean word){
+      this.next = new Node[27];
+      this.exist = word;
+    }
+    public String search(String word){
+      int key = Character.getNumericValue(word.charAt(0));
+      String a, b, c;
+      a = b = c = "";
+      if(word.length() == 1){
+        if(this.next[(key - 1) * 3] != null)
+          a = Character.toString(codeToChar((key - 1) * 3 + 1)) + " ";
+        if(this.next[((key - 1) * 3) + 1] != null)
+          b = Character.toString(codeToChar((key - 1) * 3 + 2)) + " ";
+        if(this.next[((key - 1) * 3) + 2] != null)
+          c = Character.toString(codeToChar((key - 1) * 3 + 3)) + " ";
+      }
+      else if(word.length() > 1){
+        if(this.next[(key - 1) * 3] != null){
+          a = this.next[(key - 1) * 3].search(word.substring(1, word.length()));
+          if(a.length() != 0)
+            a = Character.toString(codeToChar((key - 1) * 3 + 1)) + a;
+
+        }
+        if(this.next[((key - 1) * 3 + 1)] != null){
+          b = this.next[((key - 1) * 3 + 1)].search(word.substring(1, word.length()));
+          if(b.length() != 0)
+            b = Character.toString(codeToChar((key - 1) * 3 + 2)) + b;
+
+        }
+        if(this.next[((key - 1) * 3 + 2)] != null){
+          c = this.next[((key - 1) * 3 + 2)].search(word.substring(1, word.length()));
+          if(c.length() != 0)
+            c = Character.toString(codeToChar((key - 1) * 3 + 3)) + c;
+        }
+      }
+      /*else if(word.length() > 1){
+        if(this.next[(key - 1) * 3] != null){
+          a = Character.toString(codeToChar((key - 1) * 3 + 1));
+          a += this.next[(key - 1) * 3].search(word.substring(1, word.length()));
+        }
+        if(this.next[((key - 1) * 3 + 1)] != null){
+          b = Character.toString(codeToChar((key - 1) * 3 + 2));
+          b += this.next[((key - 1) * 3 + 1)].search(word.substring(1, word.length()));
+        }
+        if(this.next[((key - 1) * 3 + 2)] != null){
+          c = Character.toString(codeToChar((key - 1) * 3 + 3));
+          c += this.next[((key - 1) * 3 + 2)].search(word.substring(1, word.length()));
+        }
+      }*/
+      return a + b + c;
+    }
+  }
+
+  public int charToCode(char letter){//returns 1 - 27 for respective letter
     if((int) letter == 246)//ö
       return 27;
     else if((int) letter == 228)//ä
@@ -13,18 +131,25 @@ class Trie{
     return (int) letter - 96;
   }
   public char codeToChar(int code){
-    char oo = (char) 246;
     if(code == 27)//ö
       return (char) 246;
     else if(code == 26)//ä
       return (char) 228;
     else if(code == 25)//å
       return (char) 229;
-    else
-      return (char) (code + 96);
+    if(code > 16)
+      code++;
+    if(code > 21)
+      code++;
+    return (char) (code + 96);
   }
-  public int keyToIndex(int key){
-    return key - 1;
+  public int codeToIndex(int code){
+    code -= 1;
+    return code;
+  }
+  public int indexToCode(int index){
+    index += 1;
+    return index;
   }
   public int charToKey(char letter){
     int code = charToCode(letter);
@@ -35,15 +160,5 @@ class Trie{
     if(code > 24)
       code += 2;*/
     return (int) (code + 2)/3;
-  }
-
-  public class Node{
-    public Node[] next;
-    public boolean word;
-
-    public Node(){
-      this.next = new Node[27];
-      this.word = false;
-    }
   }
 }
