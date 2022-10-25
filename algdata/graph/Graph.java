3324c1e6b4b2d4efdafa23acade9bfacdb02c09b
Syncopated90 <fredrikehne@yahoo.se>
2022-10-25 17:03:05 +0200

    everything more or less working, some weird bugs left, eg Stockholm-Ånge

--- algdata/graph/Graph.java
+++ algdata/graph/Graph.java
@@ -2,10 +2,13 @@ class Graph{
   public static void main(String[] args){
     Map map = new Map("trains.csv");
     Naive naive = new Naive(map);
+    Paths paths = new Paths(map);
     map.lookup(args[0]).printConnections();
     System.out.println();
     map.lookup(args[1]).printConnections();
-    naive.bench(args[0], args[1], Integer.parseInt(args[2]));
+    System.out.println();
+    //naive.bench(args[0], args[1], Integer.parseInt(args[2]));
+    paths.bench(args[0], args[1], Integer.parseInt(args[2]));
     //System.out.println(map.lookup(args[0]).toString());
     //System.out.println(map.toString());
   }
--- algdata/graph/Paths.java
+++ algdata/graph/Paths.java
@@ -1,34 +1,48 @@
-class Naive{
+class Paths{
+  City[] path;
+  int sp;
   Map map;
-  public Naive(Map map){
+  static final int MAX = 1_000_000;
+  public Paths(Map map){
     this.map = map;
+    this.path = new City[54];
+    sp = 0;
   }
   public void bench(String from, String to, int max){
     long t0 = System.nanoTime();
     int distance = shortest(this.map.lookup(from), this.map.lookup(to), max);
     long min = System.nanoTime() - t0;
-    System.out.println("Shortest naive:" + distance + " in " + min/1000 + " us");
+    System.out.println("Shortest paths:" + distance + " in " + min/1000 + " us");
   }
   private int shortest(City from, City to, int max){
     if(max < 0)
-      return 1_000_000;
+      return MAX;
     if(from.equals(to))
-      return 1_000_000;
-    int shrt = 1_000_000;
+      return MAX;
+
+    int shrt = MAX;
     int shortestConn = 0;
+  outerLoop:
     for(int i = 0; i < from.neighbors.length; i++){
       if(from.neighbors[i] != null){
         Connection conn = from.neighbors[i];
         if(conn.city.equals(to))
           return conn.distance;
+        for(int j = 0; j < sp; j++){
+          if(path[j] != null && path[j].equals(from) == true)
+            continue outerLoop;
+        }
+        this.path[sp++] = from;
         int dist = shortest(conn.city, to, max - conn.distance);
         if(dist < shrt){
           shrt = dist;
           shortestConn = conn.distance;
           //max = shrt;
         }
+        this.path[sp--] = null;
       }
     }
+
     return shrt + shortestConn;
   }
 }
